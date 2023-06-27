3f64a2c7c8563f7f14ad0775ef571eedd0674795
BarnabasTaye <barnabasashagari@gmail.com>
2023-06-27 13:42:45 -0700

    update

--- main.h
+++ main.h
@@ -5,73 +5,73 @@
 #include <stdarg.h>
 
 /**
- * struct Flags - Struct containing flags to "turn on"
- * when a flag specifier is passed to custom_printf()
- * @plus: Flag for the '+' character
- * @space: Flag for the ' ' character
- * @hash: Flag for the '#' character
+ * struct Flags - struct containing flags to enable specific options
+ * when a flag specifier is passed to print_formatted()
+ * @plus: flag for the '+' character
+ * @space: flag for the ' ' character
+ * @hash: flag for the '#' character
  */
 typedef struct Flags
 {
-	int plus;
-	int space;
-	int hash;
-} flags_t;
+    int plus;
+    int space;
+    int hash;
+} Flags_t;
 
 /**
- * struct PrintHandler - Struct to choose the right function depending
- * on the format specifier passed to custom_printf()
- * @specifier: Format specifier
- * @handler: Pointer to the correct printing function
+ * struct PrintHandler - struct to choose the appropriate function based on
+ * the format specifier passed to print_formatted()
+ * @specifier: format specifier
+ * @print_function: pointer to the correct printing function
  */
 typedef struct PrintHandler
 {
-	char specifier;
-	int (*handler)(va_list ap, flags_t *flags);
-} print_handler_t;
+    char specifier;
+    int (*print_function)(va_list args, Flags_t *flags);
+} PrintHandler_t;
 
 /* print_numbers */
-int print_integer(va_list arg, flags_t *flags);
-void print_number(int n);
-int print_unsigned_integer(va_list arg, flags_t *flags);
+int print_integer(va_list args, Flags_t *flags);
+void print_number(int num);
+int print_unsigned_integer(va_list args, Flags_t *flags);
 int count_digits(int num);
 
 /* print_bases */
-int print_hexadecimal(va_list arg, flags_t *flags);
-int print_big_hexadecimal(va_list arg, flags_t *flags);
-int print_binary(va_list arg, flags_t *flags);
-int print_octal(va_list arg, flags_t *flags);
+int print_hexadecimal(va_list args, Flags_t *flags);
+int print_hexadecimal_uppercase(va_list args, Flags_t *flags);
+int print_binary(va_list args, Flags_t *flags);
+int print_octal(va_list args, Flags_t *flags);
 
 /* converter */
 char *convert_to_base(unsigned long int num, int base, int lowercase);
 
-/* custom_printf */
-int custom_printf(const char *format, ...);
+/* print_formatted */
+int print_formatted(const char *format, ...);
 
 /* get_print_handler */
-int (*get_print_handler(char specifier))(va_list, flags_t *);
+int (*get_print_handler(char specifier))(va_list args, Flags_t *flags);
 
 /* get_flag */
-int get_flag(char specifier, flags_t *flags);
+int get_flag(char specifier, Flags_t *flags);
 
-/* print_alpha */
-int print_string(va_list arg, flags_t *flags);
-int print_character(va_list arg, flags_t *flags);
+/* print_alphabet */
+int print_string(va_list args, Flags_t *flags);
+int print_character(va_list args, Flags_t *flags);
 
 /* write_functions */
-int custom_putchar(char c);
-int custom_puts(char *str);
+int _putchar(char c);
+int _puts(char *str);
 
 /* print_custom */
-int print_rot13(va_list arg, flags_t *flags);
-int print_reverse(va_list arg, flags_t *flags);
-int print_big_string(va_list arg, flags_t *flags);
+int print_rot13(va_list args, Flags_t *flags);
+int print_reverse(va_list args, Flags_t *flags);
+int print_string_uppercase(va_list args, Flags_t *flags);
 
 /* print_address */
-int print_address(va_list arg, flags_t *flags);
+int print_address(va_list args, Flags_t *flags);
 
 /* print_percent */
-int print_percent(va_list arg, flags_t *flags);
+int print_percent(va_list args, Flags_t *flags);
 
 #endif /* MAIN_H */
 
